39f9c4f88c24bb7007309c78305359bc52baa4f8
Angelo Ciffone <angelo.pcr@yahoo.com>
2023-05-03 17:04:41 -0400

    changes

--- app/javascript/util/api.js
+++ app/javascript/util/api.js
@@ -3,10 +3,27 @@ const csrfToken = document.querySelector("meta[name=csrf-token]").content;
 async function customFetch(url, options = {}) {
   options.headers = {
     // Your code here
+    'X-CSRF-Token': csrfToken,
+    'Accept' : 'application/JSON',
     ...options.headers
   };
+
 
   return await fetch(url, options);
 }
 
-export const foo = "bar";
+export function followUser(id) {
+  return customFetch( `/users/${id}/follow`,{
+    method: "POST",
+
+  })
+}
+
+export function unfollowUser(id) {
+  return customFetch( `/users/${id}/follow`,{
+    method: "DELETE",
+
+  })
+}
+
+
